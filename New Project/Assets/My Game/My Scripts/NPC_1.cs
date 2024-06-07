196f3e1b16c03a0e81d920570f254ff0fc65fd75
Arkudaa <71982604+Arkudaa@users.noreply.github.com>
2024-06-07 17:03:16 +0300

    v01

---
+++ New Project/Assets/My Game/My Scripts/NPC_1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+public class NPC_1 : NPC {
+	 
+	// Use this for initialization
+	public override void OnSetUpDialogue() {
+        Speech.AddDialogue("0", "Welcome to our planet!", "1");
+        Speech.AddDialogue("1", "Help us, please!", "2");
+        Speech.AddDialogue("2", "We have been attacked by monsters!", "3");
+        Speech.AddDialogue("3", "Go to the second island and defeat them all","4");
+        Speech.AddDialogue("4", "On the third island is the final tower.");
+    }
+
+    // Triggered when the player comes in range of the NPC
+    public override void OnTriggerNPC( Collider other ){
+        Speech.SetDialogue("0");
+	}
+}
---
+++ New Project/Assets/My Game/My Scripts/PlayerScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public int coins;
+
+	
+
+	// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
+	protected void Start() {
+		
+	}
+
+	// OnTriggerEnter is called when the Collider "collided" enters the trigger.
+	protected void OnTriggerEnter(Collider collided) {
+
+		// Check for collision with coins
+		if (collided.gameObject.tag == "Coin") {
+			coins++;
+			HUD.UpdateCoinCount(coins);
+			Destroy(collided.gameObject);
+		}
+
+
+		if (collided.gameObject.name =="Powerup1")
+		{
+			Destroy(collided.gameObject);
+			Abilities.doubleJumpEnabled = true;
+		}
+
+        if (collided.gameObject.name == "Powerup2")
+        {
+            Destroy(collided.gameObject);
+            Abilities.spinAttackEnabled = true;
+        }
+
+    }
+}
